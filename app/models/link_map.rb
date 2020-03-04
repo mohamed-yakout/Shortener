e1717cf152ef297d7ca90c617c4e07e1c928e97f
Mohamed Yakout <mohamed.yakout91@gmail.com>
2020-03-04 16:14:43 +0400

    Add recursive method to save records

--- app/models/link_map.rb
+++ app/models/link_map.rb
@@ -1,2 +1,41 @@
 class LinkMap < ApplicationRecord
+  validates_presence_of :original_url
+  validates :original_url, format: URI::regexp(%w[http https])
+
+  validates_uniqueness_of :original_url, :short_data
+  validates_length_of :original_url, within: 3..255
+  validates_length_of :short_data, within: 3..255
+
+  before_validation :set_short_data
+
+  def set_short_data
+    # self.short_data = SecureRandom.uuid[0..5] if self.short_data.blank?
+    self.short_data = "5be1db" if self.short_data.blank?
+    true
+  end
+
+  # get full shorten url
+  def get_shorten_url
+
+  end
+
+  # recursive function to create shorten url
+  def save_record!
+    saved_result = begin
+      self.save!
+    rescue ActiveRecord::RecordInvalid => invalid
+      puts invalid.record.errors
+      invalid
+    end
+
+    if saved_result == true
+      return true
+    elsif saved_result.record.errors[:short_data].include?("has already been taken")
+      puts "Recursive Working!"
+      self.short_data += SecureRandom.uuid[0..2]
+      return self.save_record!
+    else
+      return saved_result
+    end
+  end
 end
